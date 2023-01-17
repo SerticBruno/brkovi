dfa83134345de85fdb5b32b62a133d8b33a8c6e0
Bruno Sertić <sertic.bruno@gmail.com>
2023-01-17 19:00:14 +0100

    404 page youtube link added

--- 404.php
+++ 404.php
@@ -1,6 +1,8 @@
 <?php 
 get_header();
 $content = get_field('404', 'option')['not_found_label'];
+$youtube = get_field('404', 'option')['youtube'];
+$errorContent = get_field('404', 'option');
 ?>
 <main id="content" role="main">
 
@@ -13,6 +15,34 @@ $content = get_field('404', 'option')['not_found_label'];
             </div>
         </div>
     </section>
+
+    
+    <div class="row justify-content-around">
+        <!-- ovdje sve pjesme iz albuma -->
+        <section class="youtube-cards">
+            <div class="container">
+                <div class="row justify-content-around youtube-list">
+                    <?php 
+                        $video = checkID($youtube);
+                        // $placeHolderLogo = '';
+                        $url = "https://www.youtube.com/embed/{$video['id']}?autoplay=0&mute=0&modestbranding=1&rel=0&fs=1&enablejsapi=1";
+                        $linkUrl = "https://www.youtube.com/watch?v={$video['id']}?autoplay=0&mute=0"
+                    ?>
+                    <div class="col-10 col-md-6 card-wrapper text-center">
+                        <a class="link-box" href="<?php echo $linkUrl ?>" target="_blank">
+                            <h3><?php echo $errorContent['youtube_card_title'] ?></h3>
+                        </a>
+                        <div class="card">
+                            <iframe src="<?php echo $url ?>">
+                            </iframe>
+                            <a href="<?php echo $linkUrl ?>" target="_blank"><?php echo $errorContent['youtube_card_description'] ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
 </main>
 <?php get_sidebar(); ?>
 
